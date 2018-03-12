fb4a3aff72c94ee131ff87b638e64cff75fae1dc
Tim Malone <tdmalone@gmail.com>
2018-03-12 18:19:21 +1100

    Minor: add inline docs; resolve linting errors

--- inc/static.php
+++ inc/static.php
@@ -12,14 +12,15 @@
  */
 
 /**
- * Hooks into the Simply Static plugin, and starts its static site generation task. Attached to eg.
+ * Hooks into the Simply Static plugin, and starts its static site generation function. Attached to
  * a REST route, this allows the static generation to be called remotely, such as during a CI job.
  * The generation is then run asynchronously in the background by Simply Static.
  *
  * TODO: Add some sort of error handling if the function call doesn't work.
  *
- * @param WP_Rest_Request $request Data from the incoming REST API request.
+ * @param WP_REST_Request $request Data from the incoming REST API request.
  * @return WP_REST_Response A simple response to the API caller.
+ * @see ::tm_ss_rest_route
  * @see https://developer.wordpress.org/reference/classes/wp_rest_request/
  */
 function tm_ss_generate( WP_REST_Request $request ) {
@@ -40,7 +41,12 @@ function tm_ss_generate( WP_REST_Request $request ) {
 } // Function tm_simply_static_generate.
 
 /**
+ * Adds a custom REST route for invoking the Static Site generation function. The route is locked
+ * down to a valid API key being provided via the X-Tm-Api-Key header.
  *
+ * @return void
+ * @see ::tm_simply_static_generate
+ * @see https://developer.wordpress.org/reference/classes/wp_rest_request
  */
 function tm_ss_rest_route() {
 
@@ -63,7 +69,12 @@ function tm_ss_rest_route() {
 add_action( 'rest_api_init', 'tm_ss_rest_route' );
 
 /**
+ * Adds a quick link to the WordPress admin bar to invoke the Simply Static generate function in
+ * one click. Should be hooked to the admin_bar_menu action. Designed to work alongside the inline
+ * JS added by tm_ss_generate_script().
  *
+ * @return void
+ * @see ::tm_ss_generate_script
  */
 function tm_ss_admin_bar() {
   global $wp_admin_bar;
@@ -81,7 +92,12 @@ function tm_ss_admin_bar() {
 add_action( 'admin_bar_menu', 'tm_ss_admin_bar', 200 );
 
 /**
+ * Adds a quick script to simulate a 'click' on the Simply Static plugin's 'Generate' button
+ * if the right conditions exist. Should be hooked to the admin_print_scripts action. Designed to
+ * work along with the admin bar menu link added by tm_ss_admin_bar().
  *
+ * @return void
+ * @see ::tm_ss_admin_bar
  */
 function tm_ss_generate_script() {
 
@@ -97,7 +113,8 @@ function tm_ss_generate_script() {
 
   <script type="text/javascript">
     window.onload = () => {
-      jQuery( '#generate' ).trigger( 'click' )
+      /* global jQuery */
+      jQuery( '#generate' ).trigger( 'click' );
     };
   </script>
 

ba2c9ab3d0f6d4119484170cabebe1b7375a12c6
Tim Malone <tdmalone@gmail.com>
2018-03-25 19:55:56 +1100

    Linting and compat fixes

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,13 @@
 
+'use strict';
+
 ( () => {
 
+  const FIRST_INDEX = 0;
+
   // Add a body class when the window has completed loading.
-  window.addEventListener( 'load', () => {
-    document.querySelectorAll( 'body' )[0].classList.add( 'loaded' );
+  window.addEventListener( 'load', function() {
+    document.querySelectorAll( 'body' )[ FIRST_INDEX ].classList.add( 'loaded' );
   });
 
 })();
